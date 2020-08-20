ea77ce8209c9d7f03785ece14f4804c5a623c2f1
Ibrahim <ibrahimkholil550@gmail.com>
2020-08-20 23:35:43 +0600

    working video call connection wstablished

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ console.log("Server running at 1337");
 
 io.on("connection", socket => {
     socket.on("join-room", (roomid, userid) => {
-        console.log(roomid, userid);
+        socket.join(roomid);
+        socket.to(roomid).broadcast.emit("user-connected", userid);
     });
     socket.emit("helloFromApi", "Hello my boy!");
 })
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,18 +1,78 @@
-import React, { useEffect } from 'react';
+import React, { Component, createRef } from 'react';
 import ioClient from 'socket.io-client';
+import Peer from 'peerjs';
+import './Video.css';
 
-const Video = (props) => {
+class Video extends Component {
 
-    useEffect(() => {
+    state = {
+        roomid: null,
+        peer: null
+    }
+
+    localVideoRef = createRef();
+    remoteVideoRef = createRef();
+
+    componentDidMount() {
         const io = ioClient('http://127.0.0.1:1337');
-        io.on("helloFromApi", (data) => console.log(data))
-    }, [])
+        const { roomid } = this.props.match.params;
+        this.setState({ roomid })
 
-    return (
-        <div>
-            Channel Created! ready for action
-        </div>
-    );
+
+        const myPeer = new Peer(undefined, {
+            host: "/",
+            port: '3001'
+        });
+
+        this.setState({ peer: myPeer })
+
+        myPeer.on('open', id => {
+            io.emit("join-room", roomid, id);
+        });
+
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: true
+        }).then(stream => {
+            this.addVideoStream(this.localVideoRef, stream);
+            myPeer.on("call", call => {
+                call.answer(stream);
+                call.on("stream", userVideoStream => {
+                    this.addVideoStream(this.remoteVideoRef, userVideoStream);
+                })
+            })
+            io.on("user-connected", userid => {
+                this.connectToNewUser(userid, stream);
+            });
+        })
+    }
+
+    addVideoStream = (vidRef, stream) => {
+        vidRef.current.srcObject = stream;
+        vidRef.current.addEventListener("loadedmetadata", () => {
+            vidRef.current.play();
+        });
+    }
+
+    connectToNewUser = (userid, stream) => {
+        const call = this.state.peer.call(userid, stream);
+        console.log(this.state)
+        call.on("stream", userVideoStream => {
+            this.addVideoStream(this.remoteVideoRef, userVideoStream);
+        });
+        call.on("close", () => {
+            this.remoteVideoRef.current.remove();
+        })
+    }
+
+    render() {
+        return (
+            <div className="video-container">
+                <video muted ref={this.localVideoRef}></video>
+                <video ref={this.remoteVideoRef}></video>
+            </div>
+        );
+    }
 }
 
 export default Video;
